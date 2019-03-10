3af1c6758bcc77e724067fe4d5789609ed121b03
Jakub Westfalewski <jakub.westfalewski@allegro.pl>
2019-03-10 21:05:46 +0100

    integration test

---
+++ test/integration-spec.js
@@ -0,0 +1,29 @@
+const Application = require('spectron').Application;
+const electronPath = require('electron');
+const path = require('path');
+const assert = require('assert');
+
+describe('Application launch', function () {
+  this.timeout(10000);
+
+  beforeEach(() => {
+    this.app = new Application({
+      path: electronPath,
+      args: [path.join(__dirname, '..')],
+      env: { 'ELECTRON_IS_DEV': true },
+    });
+    return this.app.start();
+  });
+
+  afterEach(() => {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop();
+    }
+  });
+
+  it('opens a window', () => {
+    return this.app.client.getWindowCount().then((count) => {
+      assert.equal(count, 1);
+    });
+  });
+});
