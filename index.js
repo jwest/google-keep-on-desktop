50270d384906f7b458ff20a971d4a87004d30ee7
Jakub Westfalewski <jakub.westfalewski@allegro.pl>
2019-03-15 23:48:32 +0100

    add paste from clipboard with global shortcut cmd+shift+`

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { toggleInit } = require('./src/toggle');
 const { menu } = require('./src/menu');
 
 const SHORTCUT_TOGGLE_DEFAULT = 'CommandOrControl+`';
+const SHORTCUT_PASTE_DEFAULT = 'CommandOrControl+Shift+`';
 
 let win = null;
 let tray = null;
@@ -34,6 +35,25 @@ function onClose() {
 
 const toggle = toggleInit(onOpen, onClose);
 
+const paste = () => {
+  toggle();
+
+  setTimeout(() => {
+
+    win.webContents.paste();
+
+    setTimeout(() => {
+      win.webContents.sendInputEvent({
+        type: 'keyDown',
+        modifiers: [],
+        keyCode: 'Esc',
+      });
+
+      toggle();``
+    }, 100);
+  }, 100);
+};
+
 app.dock.hide();
 
 app.on('ready', () => {
@@ -48,6 +68,11 @@ app.on('ready', () => {
   const SHORTCUT_TOGGLE = settings.get('shortcuts.toggle') || SHORTCUT_TOGGLE_DEFAULT;
   if (!globalShortcut.register(SHORTCUT_TOGGLE, () => toggle())) {
     console.log(`Shortcut ${SHORTCUT_TOGGLE} registration failed`);
+  }
+
+  const SHORTCUT_PASTE = settings.get('shortcuts.paste') || SHORTCUT_PASTE_DEFAULT;
+  if (!globalShortcut.register(SHORTCUT_PASTE, () => paste())) {
+    console.log(`Shortcut ${SHORTCUT_PASTE} registration failed`);
   }
 
   win = createWindow();
